39df37c7ec3c460e3264c62d87cdd66038f7ade2
CaioWF <caioweliton@alu.ufc.br>
2020-02-02 12:06:15 -0300

    add mongodb configuration

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const routes = require("./routes");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27107/semanaOmnistackDropbox", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
